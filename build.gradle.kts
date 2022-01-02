bec8c07b684f7a49b20dc952e7da6a201bb5bb10
Bairei Kawagishi <baireikawagishi@gmail.com>
2022-01-02 14:58:54 +0100

    Remove spring-boot-starter-actuator dependency in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
